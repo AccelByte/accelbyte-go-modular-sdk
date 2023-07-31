1f043d2ae08b16c753cbfa988e7e204e0118c841
ruli <ruli@accelbyte.net>
2023-07-31 08:26:39 +0700

    chore: add the parse access token function in old iam wrapper

--- services-api/pkg/service/iam/wrapper_oAuth20.go
+++ services-api/pkg/service/iam/wrapper_oAuth20.go
@@ -26,6 +26,8 @@ type OAuth20Service struct {
 	ConfigRepository       repository.ConfigRepository
 	TokenRepository        repository.TokenRepository
 	RefreshTokenRepository repository.RefreshTokenRepository
+
+	tokenValidation *TokenValidator
 }
 
 func (aaa *OAuth20Service) GetAuthSession() auth.Session {
