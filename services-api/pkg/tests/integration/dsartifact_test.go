42784e1667dca7adce63c21d1dfb0be161a31641
ruli sastra putri <ruli@accelbyte.net>
2024-02-16 20:37:52 +0700

    test(integration): add dsartifact integration tests

---
+++ services-api/pkg/tests/integration/dsartifact_test.go
@@ -0,0 +1,60 @@
+// Copyright (c) 2024 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+package integration_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	dsartifact "github.com/AccelByte/accelbyte-go-modular-sdk/dsartifact-sdk/pkg"
+	"github.com/AccelByte/accelbyte-go-modular-sdk/dsartifact-sdk/pkg/dsartifactclient/all_terminated_servers"
+	"github.com/AccelByte/accelbyte-go-modular-sdk/dsartifact-sdk/pkg/dsartifactclient/artifact_upload_process_queue"
+)
+
+var (
+	artifactUploadProcessQueueService = &dsartifact.ArtifactUploadProcessQueueService{
+		Client:          dsartifact.NewDsartifactClient(configRepository),
+		TokenRepository: tokenRepository,
+	}
+	allTerminatedServersService = &dsartifact.AllTerminatedServersService{
+		Client:          dsartifact.NewDsartifactClient(configRepository),
+		TokenRepository: tokenRepository,
+	}
+)
+
+func TestIntegrationListAllQueue(t *testing.T) {
+	// Login User - Arrange
+	Init()
+	// CASE List all queue
+	input := &artifact_upload_process_queue.ListAllQueueParams{
+		Namespace: namespace,
+		Limit:     &limit,
+	}
+	ok, err := artifactUploadProcessQueueService.ListAllQueueShort(input)
+	if err != nil {
+		assert.FailNow(t, err.Error())
+	}
+	// ESAC
+	// Assert
+	assert.Nil(t, err, "err should be nil")
+	assert.NotNil(t, ok, "response should not be nil")
+}
+func TestIntegrationListTerminatedServersArtifact(t *testing.T) {
+	// Login User - Arrange
+	Init()
+	// CASE List terminated servers
+	input := &all_terminated_servers.ListTerminatedServersParams{
+		Limit: &limit,
+	}
+	ok, err := allTerminatedServersService.ListTerminatedServersShort(input)
+	if err != nil {
+		assert.FailNow(t, err.Error())
+	}
+	// ESAC
+	// Assert
+	assert.Nil(t, err, "err should be nil")
+	assert.NotNil(t, ok, "response should not be nil")
+}
