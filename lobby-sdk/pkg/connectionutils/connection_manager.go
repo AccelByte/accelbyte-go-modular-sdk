b24aa8aa11559777142b744f6fbc0ec198e7e009
ruli sastra putri <ruli@accelbyte.net>
2024-09-04 15:39:30 +0700

    test(core): improve websocket connection status

--- lobby-sdk/pkg/connectionutils/connection_manager.go
+++ lobby-sdk/pkg/connectionutils/connection_manager.go
@@ -21,9 +21,20 @@ const (
 	separator = "://"
 )
 
+const (
+	Disconnected ConnectionStatus = iota
+	Connecting
+	Connected
+	Reconnecting
+	Closed
+)
+
+type ConnectionStatus int
+
 type WSConnection struct {
-	Conn *websocket.Conn
-	Mu   sync.RWMutex
+	Conn     *websocket.Conn
+	Mu       sync.RWMutex
+	MuStatus sync.RWMutex
 
 	Header http.Header
 	Data   map[string]interface{}
@@ -31,6 +42,8 @@ type WSConnection struct {
 	EnableAutoReconnect  bool
 	MaxReconnectAttempts int
 	MessageHandler       WSConnectionMessageHandler
+
+	status ConnectionStatus
 }
 
 type WSConnectionOption func(wsConn *WSConnection) error
@@ -131,12 +144,27 @@ func (c *WSConnection) Close(code int, reason string) error {
 		logrus.Error("Failed to close connection: ", err)
 	}
 
+	c.SetStatus(Disconnected)
+
 	return nil
 }
 
+func (c *WSConnection) SetStatus(status ConnectionStatus) {
+	c.MuStatus.Lock()
+	defer c.MuStatus.Unlock()
+
+	c.status = status
+}
+
+func (c *WSConnection) GetStatus() ConnectionStatus {
+	c.MuStatus.Lock()
+	defer c.MuStatus.Unlock()
+
+	return c.status
+}
+
 func (c *WSConnection) DefaultCloseHandler(code int, reason string) error {
-	//c.Mu.Lock()
-	//defer c.Mu.Unlock()
+	c.SetStatus(Disconnected)
 
 	return c.Close(code, reason)
 }
