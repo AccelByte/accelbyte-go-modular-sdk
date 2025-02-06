e2ce678e877a04b2c3e63c6f7a6d99f2dfe95a24
ruli sastra putri <ruli@accelbyte.net>
2025-02-06 11:56:24 +0700

    feat(loginqueue): integration - add login queue test

---
+++ services-api/pkg/tests/integration/loginqueue_test.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2025 AccelByte Inc. All Rights Reserved.
+// This is licensed software from AccelByte Inc, for limitations
+// and restrictions contact your company contract manager.
+
+package integration_test
+
+import (
+	loginqueue "github.com/AccelByte/accelbyte-go-modular-sdk/loginqueue-sdk/pkg"
+	"github.com/AccelByte/accelbyte-go-modular-sdk/loginqueue-sdk/pkg/loginqueueclient/admin_v1"
+	"github.com/AccelByte/accelbyte-go-modular-sdk/loginqueue-sdk/pkg/loginqueueclientmodels"
+	"github.com/AccelByte/accelbyte-go-modular-sdk/services-api/pkg/tests/integration"
+	"github.com/AccelByte/accelbyte-go-modular-sdk/services-api/pkg/utils/auth"
+	"github.com/stretchr/testify/assert"
+
+	"strings"
+	"testing"
+)
+
+var (
+	loginQueueAdminV1Service = &loginqueue.AdminV1Service{
+		Client:          loginqueue.NewLoginqueueClient(auth.DefaultConfigRepositoryImpl()),
+		TokenRepository: tokenRepository,
+	}
+	updateMaxLoginRate = int32(100)
+)
+
+func TestIntegrationLoginQueue(t *testing.T) {
+	if strings.Contains(configRepository.BaseUrl, "gamingservices.accelbyte.io") {
+		t.Skip("skip for ags starter")
+	}
+
+	// Login User - Arrange
+	Init()
+
+	// CASE Admin Get Configuration
+	get, errGet := loginQueueAdminV1Service.AdminGetConfigurationShort(&admin_v1.AdminGetConfigurationParams{
+		Namespace: integration.NamespaceTest,
+	})
+
+	// ESAC
+
+	// Assert
+	assert.Nil(t, errGet, "should be nil")
+	assert.NotNil(t, get, "should not be nil")
+
+	// CASE Admin Update Configuration
+	update, errUpdate := loginQueueAdminV1Service.AdminUpdateConfigurationShort(&admin_v1.AdminUpdateConfigurationParams{
+		Body: &loginqueueclientmodels.ApimodelsConfigurationRequest{
+			MaxLoginRate: &updateMaxLoginRate,
+		},
+		Namespace: integration.NamespaceTest,
+	})
+
+	// ESAC
+
+	// Assert
+	if errUpdate != nil {
+		assert.FailNow(t, errUpdate.Error())
+	} else {
+		assert.NotNil(t, update, "should not be nil")
+		assert.Equal(t, *update.Data.MaxLoginRate, updateMaxLoginRate)
+	}
+}
